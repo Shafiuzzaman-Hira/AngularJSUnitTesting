773bdae3f1638c4f09a1a26081f9fc43775e92fb
md.shafiuzzaman.hira@gmail.com <md.shafiuzzaman.hira@gmail.com>
2016-08-10 23:17:52 +0600

    Initial commit

---
+++ AngularUnitTest/app/controllers/calculatorController.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('calculatorApp')
+        .controller('CalculatorController', CalculatorController);
+
+    CalculatorController.$inject = [];
+
+    function CalculatorController() {
+        var vm = this;
+        vm.sum = sum;
+
+        function sum() {
+            vm.ans = vm.input1 + vm.input2;
+        }
+    }
+})();
---
+++ AngularUnitTest/test/calculatorController.js
@@ -0,0 +1,24 @@
+﻿describe('CalculatorController', function() {
+    // we work with "vm" instead of "CalculatorController" to
+    // have consistent verbiage in test and controller
+    var vm;
+
+    // register an instance of the app using ngMock
+    beforeEach(angular.mock.inject('calculatorApp'));
+
+    //get an instance of the controller
+    beforeEach(angular.mock.injector(function($controller) {
+        vm = $controller('CalculatorController', {});
+    }));
+
+    describe('sum', function() {
+        it('1+1 should be equal 2', function() {
+            vm.input1 = 1;
+            vm.input2 = 1;
+            vm.sum();
+            expect(vm.ans).toBe(2);
+        });
+    });
+
+
+});
